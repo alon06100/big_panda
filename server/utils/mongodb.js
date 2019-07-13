da9851ebc3936f8f8ca9746ace15f22dea9158d4
amaor <alon06100@gmail.com>
2019-07-13 20:24:28 +0300

    heroku

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = () => {
   const options = { useCreateIndex: true, useNewUrlParser: true };
-  mongoose.connect('mongodb://localhost/bigPanda', options).catch(e => console.log(e));
+  mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/bigPanda', options).catch(e => console.log(e));
 };
 
 module.exports = {
